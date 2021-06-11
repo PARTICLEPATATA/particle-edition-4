102a166c0a7b140f527b0a0b174afba3c613a104
PARTICLEPATATA <83458716+PARTICLEPATATA@users.noreply.github.com>
2021-06-11 16:28:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var images=[
+    "a2.png",
+    "a3.png",
+    "a4.png",
+    "a5.png",
+    "end.PNG"
+   
+];
+  var i=0;
+  function nextslide(){
+     document.getElementById("album").src=images[i]; i++;
+  }
+
+
+  var photos = [
+      "p2.png",
+      "p3.png",
+      "p4.png",
+      "p5.png",
+      "p6.png",
+      "p7.png",
+      "p8.png",
+      "end.PNG"
+
+  ];
+
+  var i=0;
+  function next(){
+     document.getElementById("mubla").src=photos[i]; i++;
+  }
+
+  $('.carousel').carousel()
+    
